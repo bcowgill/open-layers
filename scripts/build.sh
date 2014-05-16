3909cd0839d96bce162facb4a02af588f5e6bf04
Brent S.A. Cowgill <brent@blismedia.com>
2014-05-16 17:05:19 +0100

    First useful demo, able ot overlay a transparent png on the globe

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ DIST=../dist
 LIB=../lib
 HTML=../html
 CSS=../css
+DATA=../data
 OPENVER=OpenLayers-2.13.1
 OLDIST=$DIST/openlayers
 
@@ -17,8 +18,9 @@ cp $LIB/3rdparty/$OPENVER/OpenLayers.js $OLDIST
 cp -R $LIB/3rdparty/$OPENVER/img $OLDIST
 cp -R $LIB/3rdparty/$OPENVER/theme $OLDIST
 
+cp -R $DATA $DIST
 cp -R $HTML $DIST
 cp -R $CSS $DIST
 cp $LIB/vis.js $DIST/js
 
- perl -i -pne 's{openlayers(/|/lib/)OpenLayers\.js}{openlayers/OpenLayers.js}xmsg' $DIST/html/index.html
+perl -i -pne 's{openlayers(/|/lib/)OpenLayers\.js}{openlayers/OpenLayers.js}xmsg' $DIST/html/index.html
