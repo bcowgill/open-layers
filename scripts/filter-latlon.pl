84782cae0163c7113c0b6ec35eb2ee1fa8c41afc
Brent S.A. Cowgill <brent@blismedia.com>
2014-05-21 17:06:46 +0100

    filter-latlon.pl randomize the ip address from static ip list for privacy protection

--- scripts/filter-latlon.pl
+++ scripts/filter-latlon.pl
@@ -2,10 +2,17 @@
 
 use strict;
 use warnings;
+use English -no_match_vars;
 
-my $raScan = [
-	[-10.85272, 49.01426], # lon,lat min
-	[2.63250, 61.58262]    # lon,lat max
+my $raZones = [
+	# UK
+	[[-10.85272, 49.01426], # lon,lat min
+	[2.63250, 61.58262]],   # lon,lat max
+
+	# Islands near Africa to calibrate heatmap delta
+	[[8.14239, 3.43956], # lon,lat min
+	[9.97436, 4.43657]], # lon,lat max
+
 ];
 my $LON = 0;
 my $LAT = 1;
@@ -14,6 +21,40 @@ my $MAX = 1;
 my $counter =  2000;
 my $LIMIT = 0;
 
+
+sub in_the_zone
+{
+	my ($lat, $lon, $raZone) = @ARG;
+	if ($raZone->[$MIN][$LON] <= $lon
+		&& $lon <= $raZone->[$MAX][$LON]
+		&& $raZone->[$MIN][$LAT] <= $lat
+		&& $lat <= $raZone->[$MAX][$LAT])
+	{
+		return 1;
+	}
+	return 0;
+}
+
+sub in_the_zones
+{
+	my ($lat, $lon, $raZones) = @ARG;
+	foreach my $raZone (@$raZones)
+	{
+		my $in = in_the_zone($lat, $lon, $raZone);
+		return 1 if $in;
+	}
+	return 0;
+}
+
+sub r
+{
+	return int(rand(256));
+}
+
+sub some_ip
+{
+	return join('.', r(), r(), r(), r());
+}
 
 while (my $line = <>)
 {
@@ -29,12 +70,12 @@ while (my $line = <>)
 		my $lon = $2;
 		if (!$LIMIT || $counter)
 		{
-			if ($raScan->[$MIN][$LON] <= $lon
-				&& $lon <= $raScan->[$MAX][$LON]
-				&& $raScan->[$MIN][$LAT] <= $lat
-				&& $lat <= $raScan->[$MAX][$LAT])
+			if (in_the_zones($lat, $lon, $raZones))
 			{
 				--$counter;
+
+				# protect privacy by randomizing the IP address
+				$line =~ s{"(\d+\.\d+\.\d+\.\d+)"}{some_ip()}xmse;
 				print $line;
 			}
 		}
