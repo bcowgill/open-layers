84f35268f21936afb9cf6e2a52b733bc2161f8aa
Brent S.A. Cowgill <brent@blismedia.com>
2014-05-21 17:05:06 +0100

    build.sh remove vim/kate swap files from the dist/ dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,9 +19,13 @@ cp -R $LIB/3rdparty/$OPENVER/img $OLDIST
 cp -R $LIB/3rdparty/$OPENVER/theme $OLDIST
 
 cp -R $DATA $DIST
+rm $DIST/data/static_ip*.js
 cp -R $HTML $DIST
 cp -R $CSS $DIST
 cp $LIB/vis.js $DIST/js
 cp $LIB/raster.js $DIST/js
+
+find $DIST -name '*.kate-swp' -delete
+find $DIST -name '*.swp' -delete
 
 perl -i -pne 's{openlayers(/|/lib/)OpenLayers\.js}{openlayers/OpenLayers.js}xmsg' $DIST/html/index.html
